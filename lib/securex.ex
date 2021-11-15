4f8d96faf95b0b008086d9be9687f778cf554760
Dev Wasi <wasibiit@gmail.com>
2021-11-15 17:06:06 +0500

    SecureX -> added has_asssess?

--- lib/securex.ex
+++ lib/securex.ex
@@ -1,5 +1,5 @@
 defmodule SecureX do
-#  alias SecureXWeb.{ ResourceController, RoleController }
+  alias SecureX.SecureXContext, as: Context
 
   @moduledoc """
   SecureX is an Elixir Library to handle your RBAC (Role Based Access Control).
@@ -43,4 +43,41 @@ defmodule SecureX do
   iex> mix ecto.migrate
   ```
   """
+
+  @doc """
+
+  """
+  @spec has_access?(any(), string(), any()) :: boolean()
+  def has_access?(user_id, res_id, permission) when !is_nil(user_id) and !is_nil(res_id) and !is_nil(permission) do
+    case translate_permission(permission) do
+      nil -> false
+      permission ->
+        case Context.get_resource(res_id) do
+          nil -> false
+          %{id: res_id} ->
+            roles = Context.get_user_roles_by_user_id(user_id)
+            case Context.get_permission_by(res_id, roles) do
+              nil -> false
+              %{permission: per} ->
+                cond do
+                  permission == 1 and per == 1 -> true
+                  permission == 2 and per == 2 -> true
+                  permission == 3 and per == 3 -> true
+                  permission == 4 and per == 4 -> true
+                  true -> false
+                end
+            end
+        end
+    end
+  end
+
+  defp translate_permission (permission) do
+    case permission do
+      "GET" || "get" || "READ" || "read" || "1" || 1 -> 1
+      "POST" || "post" || "write" || "WRITE" || "2" || 2 -> 2
+      "UPDATE" || "update" || "edit" || "EDIT" || "3" || 3 -> 3
+      "DELETE" || "delete" || "4" || 4 -> 4
+      _ -> nil
+    end
+  end
 end
