4e098c030d51a905196fa654f30b605351db1d6f
Dev Wasi <wasibiit@gmail.com>
2021-11-15 17:33:16 +0500

    Documentation -> explainations

--- lib/securex.ex
+++ lib/securex.ex
@@ -42,10 +42,79 @@ defmodule SecureX do
   iex> "Do you want to run this migration?"
   iex> mix ecto.migrate
   ```
+  You are Now Up and Running!!!
+
+  ## Guide
+
+  You can also use SecureX as a Middleware
+
+  ## Middlewares
+  In RestApi or GraphiQL all you have to do, add a `Plug`.
+
+  ## Examples
+  ```elixir
+   #lib/plugs/securex_plug.ex
+
+    defmodule MyApp.Plugs.SecureXPlug do
+      @behaviour Plug
+
+      import Plug.Conn
+
+      def init(default), do: default
+
+      def call(conn, _) do
+        with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
+            {:ok, claims} <- MyApp.Auth.Guardian.decode_and_verify(token),
+            {:ok, user} <- MyApp.Auth.Guardian.resource_from_claims(claims),
+            {:ok, %Plug.Conn{}} <- check_permissions(conn, user) do
+      conn
+    else
+      {:error, error} ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(403, Jason.encode!(%{errors: error}))
+        |> Plug.Conn.halt()
+      _ ->
+        conn
+        |> put_resp_content_type("application/json")
+        |> send_resp(403, Jason.encode!(%{errors: ["Permission Denied"]}))
+        |> Plug.Conn.halt()
+    end
+  end
+
+  defp check_permissions(%{body_params: %{"resource" => res, "permission" => permission}} = conn, %{id: user_id}) do
+    case SecureX.has_access?(user_id, res, permission) do
+        false -> {:error, false}
+        true -> {:ok, conn}
+      end
+  end
+  defp check_permissions(_, _), do: {:error, ["Invalid Request"]}
+  end
+  ```
+
+   ## Permissions
+  Valid inputs for permissions are "POST","GET","PUT" ,"DELETE","read","write","delete","edit" as well.
+  Permissions have downward flow. i.e if you have defined permissions for a higher operation,
+  It automatically assigns them permissions for lower operations.
+  like "edit" grants permissions for all operations. their hierarchy is in this order.
+
+  ```
+    "read" < "write" < "delete" < "edit"
+    "GET" < "POST" < "DELETE" < "PUT"
+    1 < 2 < 3 < 4
+  ```
   """
 
   @doc """
+  Check if user has access.
 
+  ## Examples
+
+      iex> has_access?(1, "users", "write")
+      true
+
+      iex> has_access?(1, "Gibberish", "bad_input")
+      false
   """
   @spec has_access?(any(), string(), any()) :: boolean()
   def has_access?(user_id, res_id, permission) when !is_nil(user_id) and !is_nil(res_id) and !is_nil(permission) do
@@ -74,9 +143,9 @@ defmodule SecureX do
   defp translate_permission (permission) do
     case permission do
       "GET" || "get" || "READ" || "read" || "1" || 1 -> 1
-      "POST" || "post" || "write" || "WRITE" || "2" || 2 -> 2
-      "UPDATE" || "update" || "edit" || "EDIT" || "3" || 3 -> 3
-      "DELETE" || "delete" || "4" || 4 -> 4
+      "GET" || "get" || "READ" || "read" || "1" || 1 || "POST" || "post" || "write" || "WRITE" || "2" || 2 -> 2
+      "GET" || "get" || "READ" || "read" || "1" || 1 || "POST" || "post" || "write" || "WRITE" || "2" || 2 || "UPDATE" || "update" || "edit" || "EDIT" || "3" || 3 -> 3
+      "GET" || "get" || "READ" || "read" || "1" || 1 || "POST" || "post" || "write" || "WRITE" || "2" || 2 || "UPDATE" || "update" || "edit" || "EDIT" || "3" || 3 || "DELETE" || "delete" || "4" || 4 -> 4
       _ -> nil
     end
   end
